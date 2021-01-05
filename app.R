e8e6eff82f4bd56f7063d65f3ac7fa7b92035621
Ryan Johnson <johnsrc06@gmail.com>
2021-01-05 08:46:14 -0500

    Working penguins app

--- app.R
+++ app.R
@@ -1,47 +1,48 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Palmer Penguins: K means clustering
 
 library(shiny)
+library(palmerpenguins)
 
-# Define UI for application that draws a histogram
+penguin_vars <- c("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g")
+
+# UI ------------------------------------
 ui <- fluidPage(
-
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Palmer Penguins: K-Means Clustering"),
+    
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput('xcol', 'X Variable', penguin_vars),
+            selectInput('ycol', 'Y Variable', penguin_vars, selected = penguin_vars[2]),
+            numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        
+    mainPanel(plotOutput('plot1'))
     )
 )
 
-# Define server logic required to draw a histogram
+# Server -------------------------------------------
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # Combine the selected variables into a new data frame
+    selectedData <- reactive({
+        na.omit(penguins[, c(input$xcol, input$ycol)])
+    })
+    
+    clusters <- reactive({
+        kmeans(selectedData(), input$clusters)
+    })
+    
+    output$plot1 <- renderPlot({
+        palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+                  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+        
+        par(mar = c(5.1, 4.1, 0, 1))
+        plot(selectedData(),
+             col = clusters()$cluster,
+             pch = 20, cex = 3)
+        points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
     })
 }
 
